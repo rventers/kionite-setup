5d9c58aa21aba0ecb3c75a3c42f6841877138bba
Robert Venters <robert@venters.me>
2023-07-28 18:17:51 -0700

    User Flatpaks

--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@ PS3="Select an option: "
 OPTIONS=(
 	"Set Hostname"
 	"Remove Firefox RPMs"
-	"Install RPMs"
 	"Install Flatpaks"
 	"Steam Input Rules"
+	"All Options"
 )
+
+function all-options() {
+	setup-hostname
+	remove-firefox
+	setup-flatpaks
+	steam-input
+}
 
 function remove-firefox() {
 	echo -e "\nRemoving Firefox RPMs..."
@@ -16,29 +23,27 @@ function remove-firefox() {
 }
 
 function setup-flatpaks() {
+	echo -e "\nRemoving system Flatpaks..."
+	flatpak remote-delete fedora --force
+	flatpak remove --system --noninteractive --all
 	echo -e "\nAdding Flathub..."
-	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 	echo -e "\nInstalling flatpaks..."
-	flatpak install -y flathub $(cat flatpak-packages.txt)
+	flatpak install --assumeyes --user flathub $(cat flatpaks.txt)
 	echo ""
 }
 
 function setup-hostname() {
+	echo ""
 	read -p "Hostname: " host
 	sudo hostnamectl set-hostname $host
-	echo ""
-}
-
-function setup-rpms() {
-	echo -e "\nInstalling packages..."
-	rpm-ostree install -y $(cat rpm-packages.txt)
 	echo ""
 }
 
 function steam-input() {
         echo -e "\nAdding Steam input rules..."
         sudo wget https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -O /etc/udev/rules.d/60-steam-input.rules
-        echo ""
+        setup-hostnameecho ""
 }
 
 clear
@@ -49,9 +54,9 @@ do
 		case $REPLY in
 			1) setup-hostname; break;;
 			2) remove-firefox; break;;
-			3) setup-rpms; break;;
-			4) setup-flatpaks; break;;
-			5) steam-input; break;;
+			3) setup-flatpaks; break;;
+			4) steam-input; break;;
+			5) all-options; break;;
 			$((${#OPTIONS[@]}+1))) break 2;;
 			*) echo "Invalid option: $REPLY"; break;;
 		esac
