39f65987fdecede0c5757139c169808f70a00f37
Robert Venters <robert@venters.me>
2023-07-28 18:02:28 -0700

    Revert "Rebase to Bazzite"
    This reverts commit 2ba0b994d43e2367da104f3e98c026fc05e3b3ea.

--- setup.sh
+++ setup.sh
@@ -3,44 +3,42 @@
 PS3="Select an option: "
 OPTIONS=(
 	"Set Hostname"
+	"Remove Firefox RPMs"
+	"Install RPMs"
 	"Install Flatpaks"
 	"Steam Input Rules"
-	"Rebase to Bazzite"
-	"All options"
 )
 
-function all-options() {
-	set-hostname
-	install-flatpaks
-	steam-input
-	rebase-bazzite
+function remove-firefox() {
+	echo -e "\nRemoving Firefox RPMs..."
+	rpm-ostree override remove firefox firefox-langpacks
+	echo ""
 }
 
-function install-flatpaks() {
+function setup-flatpaks() {
 	echo -e "\nAdding Flathub..."
-	flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	echo -e "\nInstalling flatpaks..."
-	flatpak install --assumeyes --user flathub $(cat flatpaks.txt)
+	flatpak install -y flathub $(cat flatpak-packages.txt)
 	echo ""
 }
 
-function rebase-bazzite() {
-	echo -e "\nRebasing to Bazzite..."
-	podman pull ghcr.io/ublue-os/config && rpm-ostree install --assumeyes --apply-live --force-replacefiles $(find ~/.local/share/containers -name ublue-os-signing.noarch.rpm 2>/dev/null) && rpm-ostree rebase --uninstall $(rpm -q ublue-os-signing-* --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{Arch}') ostree-image-signed:docker://ghcr.io/ublue-os/bazzite:latest
-	echo ""
-}
-
-function set-hostname() {
-	echo ""
+function setup-hostname() {
 	read -p "Hostname: " host
 	sudo hostnamectl set-hostname $host
 	echo ""
 }
 
-function steam-input() {
-	echo -e "\nAdding Steam input rules..."
-	sudo wget https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -O /etc/udev/rules.d/60-steam-input.rules
+function setup-rpms() {
+	echo -e "\nInstalling packages..."
+	rpm-ostree install -y $(cat rpm-packages.txt)
 	echo ""
+}
+
+function steam-input() {
+        echo -e "\nAdding Steam input rules..."
+        sudo wget https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -O /etc/udev/rules.d/60-steam-input.rules
+        echo ""
 }
 
 clear
@@ -49,11 +47,11 @@ do
 	select OPT in "${OPTIONS[@]}" Quit
 	do
 		case $REPLY in
-			1) set-hostname; break;;
-			2) install-flatpaks; break;;
-			3) steam-input; break;;
-			4) rebase-bazzite; break;;
-			5) all-options; break;;
+			1) setup-hostname; break;;
+			2) remove-firefox; break;;
+			3) setup-rpms; break;;
+			4) setup-flatpaks; break;;
+			5) steam-input; break;;
 			$((${#OPTIONS[@]}+1))) break 2;;
 			*) echo "Invalid option: $REPLY"; break;;
 		esac
