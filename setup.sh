f3454e8303efd18a729ff15860913b131bc5ed2f
Robert Venters <robert@venters.me>
2023-08-03 10:20:03 -0700

    Remove Distrobox

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ OPTIONS=(
 	"Upgrade System"
 	"Remove Firefox RPMs"
 	"Install Flatpaks"
-	"Install Distrobox (bazzite-arch)"
 	"Install Steam Input Rules"
 	"All Options"
 )
@@ -16,7 +15,6 @@ function all-options() {
 	upgrade-system
 	remove-firefox
 	setup-flatpaks
-	setup-distrobox
 	steam-input
 }
 
@@ -26,25 +24,14 @@ function remove-firefox() {
 	echo ""
 }
 
-function setup-distrobox() {
-	echo -e "\nSetting up Distrobox"
-	curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
-	distrobox create -i ghcr.io/ublue-os/bazzite-arch -n bazzite-arch -Y
-	for APP in lutris protontricks steam; do
-		distrobox enter -n bazzite-arch -- "distrobox-export --app $APP"
-  		distrobox enter -n bazzite-arch -- "distrobox-export --bin /usr/sbin/$APP"
-	done
-	echo ""
-}
-
 function setup-flatpaks() {
 	echo -e "\nRemoving system Flatpaks..."
 	flatpak remote-delete fedora --force
 	flatpak remove --system --noninteractive --all
 	echo -e "\nAdding Flathub..."
-	flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	echo -e "\nInstalling flatpaks..."
-	flatpak install --assumeyes --user flathub $(cat flatpaks.txt)
+	flatpak install --assumeyes flathub $(cat flatpaks.txt)
 	echo ""
 }
 
@@ -56,9 +43,9 @@ function setup-hostname() {
 }
 
 function steam-input() {
-        echo -e "\nAdding Steam input rules..."
-        sudo wget https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -O /etc/udev/rules.d/60-steam-input.rules
-        echo ""
+	echo -e "\nAdding Steam input rules..."
+	sudo wget https://raw.githubusercontent.com/ValveSoftware/steam-devices/master/60-steam-input.rules -O /etc/udev/rules.d/60-steam-input.rules
+	echo ""
 }
 
 function upgrade-system() {
@@ -77,9 +64,8 @@ do
 			2) upgrade-system; break;;
 			3) remove-firefox; break;;
 			4) setup-flatpaks; break;;
-			5) setup-distrobox; break;;
-			6) steam-input; break;;
-			7) all-options; break 2;;
+			5) steam-input; break;;
+			6) all-options; break 2;;
 			$((${#OPTIONS[@]}+1))) break 2;;
 			*) echo "Invalid option: $REPLY"; break;;
 		esac
